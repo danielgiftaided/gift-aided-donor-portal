df2b52f8035d6996430acf0ce10958a487d24de0
danielgiftaided <daniel@giftaided.com>
2026-07-11 01:59:20 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Register       from './pages/register'
-import Login          from './pages/login'
-import ForgotPassword from './pages/forgotPassword'
-import ResetPassword  from './pages/resetPassword'
-import MfaSetup       from './pages/mfaSetup'
-import MfaChallenge   from './pages/mfaChallenge'
-import Dashboard      from './pages/dashboard'
-import GivingHistory  from './pages/givingHistory'
-import Authorisation  from './pages/authorisation'
-import Profile        from './pages/profile'
+import Register          from './pages/register'
+import Login             from './pages/login'
+import ForgotPassword    from './pages/forgotPassword'
+import ResetPassword     from './pages/resetPassword'
+import MfaSetup          from './pages/mfaSetup'
+import MfaChallenge      from './pages/mfaChallenge'
+import Dashboard         from './pages/dashboard'
+import GivingHistory     from './pages/givingHistory'
+import Authorisation     from './pages/authorisation'
+import Profile           from './pages/profile'
+import AdminLogin        from './pages/admin/adminLogin'
+import AdminMfaChallenge from './pages/admin/adminMfaChallenge'
+import AdminDashboard    from './pages/admin/adminDashboard'
+import AdminDonors       from './pages/admin/adminDonors'
+import AdminDonations    from './pages/admin/adminDonations'
+import AdminMatching     from './pages/admin/adminMatching'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public */}
+        {/* Donor public */}
         <Route path="/register"        element={<Register />} />
         <Route path="/login"           element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password"  element={<ResetPassword />} />
 
-        {/* Auth flow */}
+        {/* Donor auth flow */}
         <Route path="/mfa-setup"       element={<MfaSetup />} />
         <Route path="/mfa-challenge"   element={<MfaChallenge />} />
 
-        {/* Authenticated donor pages */}
+        {/* Donor authenticated */}
         <Route path="/dashboard"       element={<Dashboard />} />
         <Route path="/giving-history"  element={<GivingHistory />} />
         <Route path="/authorisation"   element={<Authorisation />} />
         <Route path="/profile"         element={<Profile />} />
+
+        {/* Admin */}
+        <Route path="/admin/login"         element={<AdminLogin />} />
+        <Route path="/admin/mfa-challenge" element={<AdminMfaChallenge />} />
+        <Route path="/admin"               element={<AdminDashboard />} />
+        <Route path="/admin/donors"        element={<AdminDonors />} />
+        <Route path="/admin/donations"     element={<AdminDonations />} />
+        <Route path="/admin/matching"      element={<AdminMatching />} />
 
         {/* Default */}
         <Route path="*" element={<Navigate to="/login" replace />} />
